4e04b7916dfaa510e7c47f28085fd42bbddc4b20
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2023-08-10 21:02:04 +0500

    Remove an unused comments.

--- include/text-query.hpp
+++ include/text-query.hpp
@@ -1,18 +1,3 @@
-/*
-Tasks:
-    1. It must allow the user to indicate the name of a file to process.
-    The program will store the contents of the file so that we can 
-    display the original line in which each word appears.
-    2. The program must break each line into words and remember all the
-    lines in which each word appears. When it prints the line numbers,
-    they should be presented in ascending order and contain no duplicates.
-    3. The result of querying for a particular word should be the line 
-    numbers on which that word appeared.
-    4. To print the text in which the word appeared, it must be possible to
-    fetch the line from the input file corresponding to a given line number.
-*/ 
-
-
 #ifndef __TEXT_QUERY_HPP__
 #define __TEXT_QUERY_HPP__
 
